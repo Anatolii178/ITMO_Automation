163de9fb385b27e0140c1f37ef00f558ae770f48
Anatolii178 <kesarev.ai@gmail.com>
2025-05-08 20:53:35 +0300

    2 commit

--- home_work/5_hw.py
+++ home_work/5_hw.py
@@ -3,11 +3,17 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 
-driver = webdriver.Chrome()
-driver.get('https://www.saucedemo.com/')
+def open_site_and_find_elements():
 
-icon1 = driver.find_element(By.CSS_SELECTOR, '[placeholder = "Username"]')   # #user-name
-icon2 = driver.find_element(By.CSS_SELECTOR, '[placeholder="Password"]')  # #password
-icon3 = driver.find_element(By.CSS_SELECTOR, '[type="submit"]')  # #login-button
-if (icon1 and icon2 and icon3) is not None:
-    print('Элементы найдены')
+    driver = webdriver.Chrome()
+    driver.get('https://www.saucedemo.com/')
+
+    icon1 = driver.find_element(By.CSS_SELECTOR, '[placeholder = "Username"]')   # #user-name
+    icon2 = driver.find_element(By.CSS_SELECTOR, '[placeholder="Password"]')  # #password
+    icon3 = driver.find_element(By.CSS_SELECTOR, '[type="submit"]')  # #login-button
+
+    if (icon1 and icon2 and icon3) is not None:
+        print('Элементы найдены')
+
+
+open_site_and_find_elements()
